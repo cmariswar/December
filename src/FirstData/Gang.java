eba38b446295d1755c8fda66e3e85f913f701dbf
cmariswar <cmariswar@protonmail.com>
2021-12-14 12:18:07 +0530

    TheStar

---
+++ src/FirstData/Gang.java
@@ -0,0 +1,13 @@
+package FirstData;
+
+public class Gang 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("I can do it anything");
+		System.out.println("Show me");
+		
+		
+	}
+
+}
